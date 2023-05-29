9c5ff2040a39d9c5a586be68956a6171bcfa896e
Khldoun Alhalabi <khaldoun1222@hotmail.com>
2023-05-29 13:52:56 +0300

    adding phpDocs to BaseService

--- src/Contracts/Services/BaseService.php
+++ src/Contracts/Services/BaseService.php
@@ -3,47 +3,83 @@
 namespace Cubeta\CubetaStarter\Contracts\Services;
 
 use Cubeta\CubetaStarter\Contracts\Repositories\IBaseRepository;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseService implements IBaseService
 {
-    protected $repository;
+    protected IBaseRepository $repository;
 
     public function __construct(IBaseRepository $repository)
     {
         $this->repository = $repository;
     }
 
-    public function index(array $relations = [])
+    /**
+     * @param array $relations
+     * @return mixed
+     */
+    public function index(array $relations = []): mixed
     {
         return $this->repository->all($relations);
     }
 
-    public function indexWithPagination(array $relations = [], $per_page = 10)
+    /**
+     * @param array $relations
+     * @param int $per_page
+     * @return array|null
+     */
+    public function indexWithPagination(array $relations = [], int $per_page = 10): ?array
     {
         return $this->repository->all_with_pagination($relations, $per_page);
     }
 
-    public function formatPaginationData($data)
+    /**
+     * @param $data
+     * @return array
+     */
+    public function formatPaginationData($data): array
     {
         return $this->repository->formatPaginateData($data);
     }
 
-    public function store(array $data, array $relation = [])
+    /**
+     * @param array $data
+     * @param array $relationships
+     * @return mixed
+     */
+    public function store(array $data, array $relationships = []): mixed
     {
-        return $this->repository->create($data, $relation);
+        return $this->repository->create($data, $relationships);
     }
 
-    public function view($id, array $relations = [])
+    /**
+     * @param $id
+     * @param array $relationships
+     * @return array|Builder|Collection|Model|null
+     */
+    public function view($id, array $relationships = []): Model|Collection|Builder|array|null
     {
-        return $this->repository->find($id, $relations);
+        return $this->repository->find($id, $relationships);
     }
 
-    public function update(array $data, $id, array $relation = [])
+    /**
+     * @param array $data
+     * @param $id
+     * @param array $relationships
+     * @return mixed
+     */
+    public function update(array $data, $id, array $relationships = []): mixed
     {
-        return $this->repository->update($data, $id, $relation);
+        return $this->repository->update($data, $id, $relationships);
     }
 
-    public function delete($id)
+    /**
+     * @param $id
+     * @return bool|null
+     */
+    public function delete($id): ?bool
     {
         return $this->repository->delete($id);
     }
--- src/Contracts/Services/IBaseService.php
+++ src/Contracts/Services/IBaseService.php
@@ -2,19 +2,52 @@
 
 namespace Cubeta\CubetaStarter\Contracts\Services;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
 interface IBaseService
 {
     public function index(array $relations = []);
 
-    public function indexWithPagination(array $relations = [], $per_page = 10);
+    /**
+     * @param array $relations
+     * @param int $per_page
+     * @return array|null
+     */
+    public function indexWithPagination(array $relations = [], int $per_page = 10): ?array;
 
-    public function formatPaginationData($data);
+    /**
+     * @param $data
+     * @return array
+     */
+    public function formatPaginationData($data): array;
 
-    public function store(array $data, array $relations = []);
+    /**
+     * @param array $data
+     * @param array $relationships
+     * @return mixed
+     */
+    public function store(array $data, array $relationships = []): mixed;
 
-    public function view($id, array $relations = []);
+    /**
+     * @param $id
+     * @param array $relationships
+     * @return Model|Collection|Builder|array|null
+     */
+    public function view($id, array $relationships = []): Model|Collection|Builder|array|null;
 
-    public function update(array $data, $id, array $relations = []);
+    /**
+     * @param array $data
+     * @param $id
+     * @param array $relationships
+     * @return mixed
+     */
+    public function update(array $data, $id, array $relationships = []): mixed;
 
-    public function delete($id);
+    /**
+     * @param $id
+     * @return bool|null
+     */
+    public function delete($id): ?bool;
 }
