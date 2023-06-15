b874a444731eb3723b42a30c64f84c8f5472d2d0
Khldoun Alhalabi <khaldoun1222@hotmail.com>
2023-06-15 17:38:35 +0300

    multi language input field

--- resources/views/components/form/checkboxes/form-check-radio.blade.php
+++ resources/views/components/form/checkboxes/form-check-radio.blade.php
@@ -1,44 +1,46 @@
-@props(['name' , 'value' , 'checked' => false])
-<div class="form-check">
-    <input class="form-check-input @error(columnNaming($name)) is-invalid @enderror" type="radio"
-           name="{{columnNaming($name)}}"
-           id="{{$name}}-{{$value}}-radio" value="{{$value}}" {{$attributes->merge()}} @checked($checked)>
-    <label class="form-check-label" for="{{$name}}-{{$value}}-radio">
-        @if(is_bool($value))
+@props(['name', 'value', 'checked' => 'false'])
+@if (is_array($value))
 
-            @if($value)
-                {{ucfirst($name)}}
-            @else
-                Not {{ucfirst($name)}}
-            @endif
-
-        @else
-            {{ucfirst($value)}}
-        @endif
-    </label>
-</div>
-
-@if(is_array($value))
-    @foreach($value as $v)
+    @foreach ($value as $v)
         <div class="form-check">
             <input class="form-check-input @error(columnNaming($name)) is-invalid @enderror" type="radio"
-                   name="{{columnNaming($name)}}"
-                   id="{{$name}}-{{$v}}-radio" value="{{$v}}"
-                   {{$attributes->merge()}}
-                   @if(!is_bool($checked) && $checked == $v)checked@endif>
-            <label class="form-check-label" for="{{$name}}-{{$v}}-radio">
-                @if(is_bool($v))
-
-                    @if($v)
-                        {{ucfirst($name)}}
+                   name="{{ columnNaming($name) }}" id="{{ $name }}-{{ $v }}-radio"
+                   value="{{ $v }}" {{ $attributes->merge() }}
+                   @if ($checked == $v)
+                       checked
+                @endif>
+            <label class="form-check-label" for="{{ $name }}-{{ $v }}-radio">
+                @if ($v == 'true' || $v == 'false' || $v == '1' || $v == '0')
+                    @if ($v == 'true' || $v == '1')
+                        {{ ucfirst(Str::headline(Str::studly($name))) }}
                     @else
-                        Not {{ucfirst($name)}}
+                        {{ Str::startsWith($name, 'is_') ? ucfirst(Str::headline(Str::studly(str_replace('is_', "isn't ", $name)))) : 'Not' . ucfirst(Str::headline(Str::studly($name))) }}
                     @endif
-
                 @else
-                    {{ucfirst($v)}}
+                    {{ ucfirst($v) }}
                 @endif
             </label>
         </div>
     @endforeach
+@else
+    <div class="form-check">
+        <input class="form-check-input @error(columnNaming($name)) is-invalid @enderror" type="radio"
+               name="{{ columnNaming($name) }}" id="{{ $name }}-{{ $value }}-radio"
+               value="{{ $value }}" {{ $attributes->merge() }}
+               @if ($checked == $value || $checked == 'true' || $checked == '1')
+                   checked
+            @endif>
+        <label class="form-check-label" for="{{ $name }}-{{ $value }}-radio">
+            @if ($value == 'true' || $value == 'false' || $value == '1' || $value == '0')
+
+                @if ($value == 'true' || $value == '1')
+                    {{ ucfirst(Str::headline(Str::studly($name))) }}
+                @else
+                    {{ Str::startsWith($name, 'is_') ? ucfirst(Str::headline(Str::studly(str_replace('is_', "isn't ", $name)))) : 'Not' . ucfirst(Str::headline(Str::studly($name))) }}
+                @endif
+            @else
+                {{ ucfirst($value) }}
+            @endif
+        </label>
+    </div>
 @endif
