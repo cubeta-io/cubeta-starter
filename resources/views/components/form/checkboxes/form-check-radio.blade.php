32daf0ebab95a574becfa4477564120fead82582
Khldoun Alhalabi <khaldoun1222@hotmail.com>
2023-06-15 16:23:34 +0300

    multi language input field

--- resources/views/components/form/checkboxes/form-check-radio.blade.php
+++ resources/views/components/form/checkboxes/form-check-radio.blade.php
@@ -1,9 +1,9 @@
 @props(['name' , 'value' , 'checked' => false])
 <div class="form-check">
     <input class="form-check-input @error(columnNaming($name)) is-invalid @enderror" type="radio"
-           name="{{$name}}"
-           id="{{$value}}-radio" value="{{$value}}" {{$attributes->merge()}} @checked($checked)>
-    <label class="form-check-label" for="{{$value}}-radio">
+           name="{{columnNaming($name)}}"
+           id="{{$name}}-{{$value}}-radio" value="{{$value}}" {{$attributes->merge()}} @checked($checked)>
+    <label class="form-check-label" for="{{$name}}-{{$value}}-radio">
         @if(is_bool($value))
 
             @if($value)
@@ -17,3 +17,28 @@
         @endif
     </label>
 </div>
+
+@if(is_array($value))
+    @foreach($value as $v)
+        <div class="form-check">
+            <input class="form-check-input @error(columnNaming($name)) is-invalid @enderror" type="radio"
+                   name="{{columnNaming($name)}}"
+                   id="{{$name}}-{{$v}}-radio" value="{{$v}}"
+                   {{$attributes->merge()}}
+                   @if(!is_bool($checked) && $checked == $v)checked@endif>
+            <label class="form-check-label" for="{{$name}}-{{$v}}-radio">
+                @if(is_bool($v))
+
+                    @if($v)
+                        {{ucfirst($name)}}
+                    @else
+                        Not {{ucfirst($name)}}
+                    @endif
+
+                @else
+                    {{ucfirst($v)}}
+                @endif
+            </label>
+        </div>
+    @endforeach
+@endif
