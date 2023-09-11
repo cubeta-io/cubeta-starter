83590488dc631cdddd521b086f3060752ffdf624
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2023-09-11 14:42:08 +0300

    fixes

--- src/Commands/PublishAllAssetsCommand.php
+++ src/Commands/PublishAllAssetsCommand.php
@@ -18,30 +18,47 @@ class PublishAllAssetsCommand extends Command
             'cubeta-starter-services',
             'cubeta-starter-api-controller',
             'cubeta-starter-middlewares',
-            'cubeta-starter-helpers',
             'cubeta-starter-validation-rules',
             'cubeta-starter-traits',
             'cubeta-starter-config',
-            'cubeta-starter-handler',
             'cubeta-starter-assets',
+            'cubeta-starter-providers'
         ];
 
         $output = "";
 
         foreach ($tags as $tag) {
-            if ($tag = 'cubeta-starter-handler') {
-                Artisan::call('vendor:publish', [
-                    '--tag' => 'cubeta-starter-handler',
-                    '--force' => true
-                ]);
-            } else {
-                Artisan::call('vendor:publish', [
-                    '--tag' => $tag,
-                ]);
-            }
+            Artisan::call('vendor:publish', [
+                '--tag' => $tag,
+            ]);
             $output = $output . "\n" . Artisan::output();
         }
 
+        $this->addSetLocalRoute();
+
+        Artisan::call('vendor:publish', [
+            '--tag' => 'cubeta-starter-handler',
+            '--force' => true
+        ]);
+        $output . "\n" . Artisan::output();
+
         $this->info($output);
+    }
+
+    private function addSetLocalRoute()
+    {
+        if (file_exists(base_path('app/Http/Controllers/SetLocaleController.php'))) {
+            $route = "Route::post('/locale', [\App\Http\Controllers\SetLocaleController::class, 'setLanguage'])->middleware('web')->name('set-locale');";
+        } else {
+            $route = "Route::post('/blank', function () {
+                    return response()->noContent();
+                })->middleware('web')->name('set-locale');";
+        }
+
+        $routePath = base_path("routes/web.php");
+
+        if (file_exists($routePath)) {
+            file_put_contents($routePath, $route, FILE_APPEND);
+        }
     }
 }
