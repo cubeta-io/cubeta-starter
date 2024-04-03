01fc4cb0dcdd1aa5d6f5dd9f887d557b3c0b2d85
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-04-03 13:53:35 +0300

    fixes

--- resources/views/components/export-button.blade.php
+++ resources/views/components/export-button.blade.php
@@ -9,7 +9,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="export-modal-label">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="export-modal-label">Export To Excel</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
--- resources/views/components/import-button.blade.php
+++ resources/views/components/import-button.blade.php
@@ -14,7 +14,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="importModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="importModalLabel">Import From Excel</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                             aria-label="Close"></button>
                 </div>
