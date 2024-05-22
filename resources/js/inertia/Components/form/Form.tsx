fd239fa057f81ec8c9c7edf3b534097d7404bb84
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-05-22 15:17:55 +0300

    generating inertia : 1 - adding buttonText property to Form component 2 - removing the blue background of input auto completing by chrome

--- resources/js/inertia/Components/form/Form.tsx
+++ resources/js/inertia/Components/form/Form.tsx
@@ -2,24 +2,26 @@ import { FormEvent, ReactNode } from "react";
 import Button from "../ui/Button";
 
 const Form = ({
-  onSubmit,
-  processing,
-  children,
-}: {
-  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
-  processing?: boolean;
-  children?: ReactNode;
+                  onSubmit,
+                  processing,
+                  children,
+                  buttonText = "submit",
+              }: {
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    buttonText?: string;
+    processing?: boolean;
+    children?: ReactNode;
 }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      {children}
-      <div className={"flex items-center justify-center my-2"}>
-        <Button type="submit" disabled={processing}>
-          Submit
-        </Button>
-      </div>
-    </form>
-  );
+    return (
+        <form onSubmit={onSubmit}>
+            {children}
+            <div className={"flex items-center justify-center my-2"}>
+                <Button type="submit" disabled={processing}>
+                    {buttonText}
+                </Button>
+            </div>
+        </form>
+    );
 };
 
 export default Form;
