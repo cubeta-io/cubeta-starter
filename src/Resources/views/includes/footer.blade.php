4c1b01b2c09fb721c9ee37e7f26555f4ad691788
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2024-08-15 15:24:27 +0300

    remove unwanted dependencies

--- src/Resources/views/includes/footer.blade.php
+++ src/Resources/views/includes/footer.blade.php
@@ -3,26 +3,3 @@
     <script src="{{asset("$assetsPath/js/jquery-3.7.1.min.js")}}"></script>
     @stack('scripts')
 </footer>
-
-
-<script type="module">
-    $(document).ready(function () {
-        const myModal = document.getElementById('spinner');
-        const modal = new bootstrap.Modal(myModal, {
-            keyboard: false,
-            backdrop: "static"
-        });
-        modal.hide();
-
-        $('.submit-button').click(function () {
-            document.getElementById('modal-title').innerText = "Please wait while we generate your files ..."
-            modal.show();
-        })
-
-        $(document).on('keydown', function (event) {
-            if (event.key === 'Escape') {
-                modal.hide();
-            }
-        });
-    });
-</script>
