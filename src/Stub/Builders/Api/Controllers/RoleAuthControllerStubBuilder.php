5c218cd2a2723b7a11ad3efd95424f73ede93838
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-04-17 11:21:36 +0300

    Refactor: update RoleAuthController stub to use double curly braces for placeholders and clean up unused properties in RoleAuthControllerStubBuilder.

--- src/Stub/Builders/Api/Controllers/RoleAuthControllerStubBuilder.php
+++ src/Stub/Builders/Api/Controllers/RoleAuthControllerStubBuilder.php
@@ -4,44 +4,16 @@ namespace Cubeta\CubetaStarter\Stub\Builders\Api\Controllers;
 
 use Cubeta\CubetaStarter\Helpers\CubePath;
 use Cubeta\CubetaStarter\Stub\Contracts\ClassStubBuilder;
-use Illuminate\Support\Str;
 
+/**
+ * @method self serviceNamespace(string $serviceNamespace)
+ * @method self role(string $role)
+ * @method self roleEnumName(string $roleEnumName)
+ */
 class RoleAuthControllerStubBuilder extends ClassStubBuilder
 {
-    private string $serviceNamespace;
-    private string $role;
-    private string $roleEnumName;
-
-    public function serviceNamespace(string $serviceNamespace): static
-    {
-        $this->serviceNamespace = $serviceNamespace;
-        return $this;
-    }
-
-    public function role(string $role): static
-    {
-        $this->role = ucfirst(Str::singular(Str::studly($role)));
-        return $this;
-    }
-
-    public function roleEnumName(string $roleEnumName): static
-    {
-        $this->roleEnumName = $roleEnumName;
-        return $this;
-    }
-
     protected function stubPath(): string
     {
         return CubePath::stubPath('Api/Controllers/RoleAuthController.stub');
-    }
-
-    protected function getStubPropertyArray(): array
-    {
-        return [
-            '{namespace}' => $this->namespace,
-            '{service_namespace}' => $this->serviceNamespace,
-            '{role}' => $this->role,
-            '{role_enum_name}' => $this->roleEnumName,
-        ];
     }
 }
