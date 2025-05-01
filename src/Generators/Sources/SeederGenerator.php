f9fd7f67bb73ad5e1108f95fdcf0ef29927244e1
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-01 11:55:26 +0300

    Refactor: Add SeederStubBuilder and enhance seeder generation workflow.
    Introduced `SeederStubBuilder` for modular and reusable seeder generation. Enhanced `SeederGenerator` to utilize the builder, improving code clarity and flexibility. Updated logic to ensure seeders are properly registered in `DatabaseSeeder`.

--- src/Generators/Sources/SeederGenerator.php
+++ src/Generators/Sources/SeederGenerator.php
@@ -2,9 +2,10 @@
 
 namespace Cubeta\CubetaStarter\Generators\Sources;
 
-
 use Cubeta\CubetaStarter\Generators\AbstractGenerator;
 use Cubeta\CubetaStarter\Helpers\CubePath;
+use Cubeta\CubetaStarter\Logs\CubeLog;
+use Cubeta\CubetaStarter\Stub\Builders\Seeders\SeederStubBuilder;
 
 class SeederGenerator extends AbstractGenerator
 {
@@ -12,28 +13,64 @@ class SeederGenerator extends AbstractGenerator
 
     public function run(bool $override = false): void
     {
-
         $seederPath = $this->table->getSeederPath();
+        SeederStubBuilder::make()
+            ->modelNamespace($this->table->getModelNamespace(false))
+            ->modelName($this->table->modelNaming())
+            ->generate($seederPath, $this->override);
 
-        if ($seederPath->exist()) {
-            $seederPath->logAlreadyExist("Generating Seeder For ({$this->table->modelName}) Model");
-            return;
-        }
-
-        $seederPath->ensureDirectoryExists();
-
-        $stubProperties = [
-            '{modelName}'      => $this->table->modelName,
-            '{modelNamespace}' => config('cubeta-starter.model_namespace'),
-        ];
-
-        $this->generateFileFromStub($stubProperties, $seederPath->fullPath);
-
-        $seederPath->format();
+        $this->callInDatabaseSeeder();
     }
 
-    protected function stubsPath(): string
+    /**
+     * Ensures that a specific seeder class call is added to the `DatabaseSeeder` file within the `run` method.
+     * If it does not already exist, it appends the seeder class call to the list of existing seeders or creates the
+     * seeder call if it's missing entirely.
+     * @return void
+     */
+    private function callInDatabaseSeeder(): void
     {
-        return CubePath::stubPath('seeder.stub');
+        $dbSeederPath = CubePath::make('database/seeders/DatabaseSeeder.php');
+        $seederName = $this->table->getSeederName();
+        $seederCall = $this->table->getSeederClassString() . "::class";
+
+        if (!$dbSeederPath->exist()) {
+            return;
+        }
+        $seederContent = $dbSeederPath->getContent();
+        $pattern = '/public\s*function\s*run\s*\(\s*\)\s*(?::\s*void)?\s*\{(.*?)\$this\s*->\s*call\(\s*\[(.*?)]\s*\);(.*?)}/s';
+        if (preg_match($pattern, $seederContent, $matches)) {
+            $exactMatch = $matches[2] ?? null;
+            if (str($exactMatch)->contains($seederName)) {
+                return;
+            }
+
+            if (!empty($exactMatch)) {
+                if (str($exactMatch)->trim()->endsWith(',')) {
+                    $replace = "$exactMatch\n$seederCall,\n";
+                } else {
+                    $replace = "$exactMatch,\n$seederCall,\n";
+                }
+
+                $seederContent = str_replace($exactMatch, $replace, $seederContent);
+                $dbSeederPath->putContent($seederContent);
+                CubeLog::contentAppended($seederCall, $dbSeederPath->fullPath);
+                $dbSeederPath->format();
+                return;
+            }
+        }
+
+        $pattern = '/public\s*function\s*run\s*\(\s*\)\s*(?::\s*void)?\s*\{(.*?)}/s';
+        if (preg_match($pattern, $seederContent, $matches)) {
+            $exactMatch = $matches[1] ?? null;
+            if (!empty($exactMatch)) {
+                $newContent = "\$this->call([\n$seederCall,\n]);\n";
+                $replace = "$exactMatch\n$newContent";
+                $seederContent = str_replace($exactMatch, $replace, $seederContent);
+                $dbSeederPath->putContent($seederContent);
+                CubeLog::contentAppended($newContent, $dbSeederPath->fullPath);
+                $dbSeederPath->format();
+            }
+        }
     }
 }
