6e13c5534971ea5ab11fcb14ae8c28cff816623a
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-13 12:03:40 +0300

    Fix: fixing react navbar and sidebar styles

--- resources/js/inertia/Components/ui/Navbar.tsx
+++ resources/js/inertia/Components/ui/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = ({
     <nav className="bg-white-secondary dark:bg-dark-secondary sticky top-0 z-30 flex max-h-20 w-full items-center justify-between px-3 shadow-md">
       <div className={`flex w-full items-center gap-1`}>
         {!isSidebarOpen && (
-          <button type={"button"} onClick={() => toggleSidebar()}>
+          <button className="cursor-pointer" type={"button"} onClick={() => toggleSidebar()}>
             <Menu className="text-brand h-8 w-8 dark:text-white" />
           </button>
         )}
