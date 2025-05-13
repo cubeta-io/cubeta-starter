f737c2f7ffa4e5c1f9ef96d3f6726384c78f99b4
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-13 15:59:02 +0300

    Fix: enhance LanguageDropDown.tsx component

--- resources/js/inertia/Components/ui/LanguageDropDown.tsx
+++ resources/js/inertia/Components/ui/LanguageDropDown.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-import { usePage } from "@inertiajs/react";
-import { MiddlewareProps } from "@/types";
+import LoadingSpinner from "@/Components/icons/LoadingSpinner";
 import { AvailableLocales } from "@/Models/Translatable";
+import Http from "@/Modules/Http/Http";
+import { MiddlewareProps } from "@/types";
+import { usePage } from "@inertiajs/react";
+import { useEffect, useRef, useState } from "react";
 
 const LanguageDropdown = () => {
   const { currentLocale, availableLocales, csrfToken } =
@@ -10,6 +12,8 @@ const LanguageDropdown = () => {
   const [selectedLocale, setSelectedLocale] = useState<
     string | AvailableLocales
   >(currentLocale as string);
+
+  const [loading, setLoading] = useState(false);
 
   const selectorRef = useRef<HTMLDivElement>(null);
 
@@ -29,62 +33,75 @@ const LanguageDropdown = () => {
     };
   }, []);
 
+  const handleLocalChange = (locale: string) => {
+    setLoading(true);
+    Http.make()
+      .post(
+        route("set-locale"),
+        {
+          lang: locale,
+        },
+        {
+          "X-CSRF-TOKEN": `${csrfToken}`,
+        },
+      )
+      .then(() => {
+        setLoading(false);
+        window.localStorage.setItem("locale", locale);
+        location.reload();
+      });
+    setSelectedLocale(locale as AvailableLocales);
+    setOpen(false);
+  };
+
   return (
     <div ref={selectorRef} className="relative w-auto cursor-pointer">
-      <div
-        className="bg-white-secondary dark:bg-dark-secondary inline-flex items-center overflow-hidden rounded-md"
-        onClick={() => setOpen((prevState) => !prevState)}
-      >
-        <div className="bg-primary flex p-2 text-sm/none text-white">
-          {selectedLocale as string}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <div
+            className="bg-white-secondary dark:bg-dark-secondary inline-flex items-center overflow-hidden rounded-md"
+            onClick={() => setOpen((prevState) => !prevState)}
           >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-      </div>
+            <div className="bg-primary flex p-2 text-sm/none text-white">
+              {selectedLocale as string}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
 
-      <div
-        className={`${open ? "absolute" : "hidden"} bg-white-secondary dark:bg-dark-secondary end-0 z-10 mt-2 w-20 rounded-md shadow-lg`}
-        role="menu"
-      >
-        <div className="p-2">
-          {(availableLocales as string[]).map((locale) => (
-            <span
-              className="block cursor-pointer rounded-lg px-4 py-2 text-sm hover:bg-gray-50 hover:text-gray-700 dark:text-white"
-              role="menuitem"
-              key={locale}
-              onClick={() => {
-                fetch(route("set-locale"), {
-                  body: JSON.stringify({
-                    lang: locale,
-                  }),
-                  headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-TOKEN": `${csrfToken}`,
-                  },
-                  method: "POST",
-                }).then(() => {
-                  window.localStorage.setItem("locale", locale);
-                  location.reload();
-                });
-                setSelectedLocale(locale as AvailableLocales);
-                setOpen(false);
-              }}
-            >
-              {locale}
-            </span>
-          ))}
-        </div>
-      </div>
+          <div
+            className={`${open ? "absolute" : "hidden"} bg-white-secondary dark:bg-dark-secondary end-0 z-10 mt-2 w-20 rounded-md shadow-lg`}
+            role="menu"
+          >
+            <div className="p-2">
+              {(availableLocales as string[]).map((locale) => (
+                <span
+                  className="block cursor-pointer rounded-lg px-4 py-2 text-sm hover:bg-gray-50 hover:text-gray-700 dark:text-white"
+                  role="menuitem"
+                  key={locale}
+                  onClick={() => {
+                    handleLocalChange(locale);
+                  }}
+                >
+                  {locale}
+                </span>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
