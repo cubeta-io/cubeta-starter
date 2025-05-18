db1e0cc5b54619fa691a041b2936c845e65d623d
Khaldoun Alhalabi <khaldoun1222@hotmail.com>
2025-05-18 16:31:34 +0300

    Enhancement: install react ts packages as a development dependencies
    this change will make the development process easier since it will provide the propper autocomplete when writing react components

--- resources/js/inertia/types/index.d.ts
+++ resources/js/inertia/types/index.d.ts
@@ -4,6 +4,7 @@ import { route as routeFn } from "ziggy-js";
 import { PageProps as InertiaProps } from "@inertiajs/core";
 
 export type MiddlewareProps<
+// @ts-ignore
   T extends InertiaProps<string, unknown> = Record<string, unknown>,
 > = T & {
   authUser: User;
